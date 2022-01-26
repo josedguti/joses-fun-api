82be3714a009696cb56565bb5a466e0f0f5f3fe2
Jose Gutierrez <78231620+josedguti@users.noreply.github.com>
2022-01-26 14:34:17 +0100

    deleted commented code

--- pages/index.js
+++ pages/index.js
@@ -24,55 +24,5 @@ export default function Home() {
         </button>
       </div>
     </>
-    // <div>
-    //   <Head>
-    //     <title>Joses's Fun Api</title>
-    //     <meta name="R&M API" content="Rick and Morty Characters" />
-    //     <link rel="icon" href="/favicon.ico" />
-    //   </Head>
-
-    //   <input
-    //     type="text"
-    //     className="input"
-    //     placeholder="Search for Character..."
-    //     onChange={(event) => {
-    //       setSearchTerm(event.target.value);
-    //     }}
-    //   />
-    //   <br />
-
-    //     <div className="square">
-    //       {characters
-    //         .filter((item) => {
-    //           if (searchTerm === "") {
-    //             return item;
-    //           } else if (
-    //             item.name.toLowerCase().includes(searchTerm.toLowerCase())
-    //           ) {
-    //             return item;
-    //           }
-    //           return false;
-    //         })
-    //         .map((item, index) => (
-    //           <div className="card">
-    //             <div className="card-image--wrapper">
-    //               <img
-    //                 className="card-image"
-    //                 src={item.image}
-    //                 alt="rick and morty pictures"
-    //               />
-    //             </div>
-    //             <div className="card-body">
-    //               <h2>{item.name}</h2>
-    //               <p>
-    //                  Gender: {item.gender} - Specie: {item.species} - Status: {item.status}
-    //               </p>
-    //               <p>Location: {item.location.name}</p>
-
-    //             </div>
-    //           </div>
-    //         ))}
-    //     </div>
-    // </div>
   );
 }
