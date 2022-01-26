b1342d32ce4d0f802776b26cefe262a9f52bac49
Jose Gutierrez <78231620+josedguti@users.noreply.github.com>
2022-01-26 22:52:15 +0100

    styled footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const navigation = [
     {
       name: 'GitHub',
-      href: 'https://github.com/josedguti',
+      href: 'https://github.com/josedguti/joses-fun-api',
       target: '_blank',
       icon: (props) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
@@ -19,22 +19,21 @@ const navigation = [
   
   export default function Footer() {
     return (
-      <footer className="bg-zinc-800 fixed inset-x-0 bottom-0 ">
-        <div className="flex justify-center py-2 ">
-          <div className="w-64 space-x-7 md:order-1">
-            {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
-                <span className="sr-only">{item.name}</span>
-                <item.icon className="h-6 w-6 ml-10" aria-hidden="true" />
-              </a>
-            ))}
-          </div>
-          <br />
-          <div className="mr-20">
-            <p className="text-center text-base text-gray-400 ">&copy; 2022 Jose&apos;s Fun API. All rights reserved.</p>
-          </div>
-        </div>
-      </footer>
+      <footer className="bg-zinc-800 fixed inset-x-0 bottom-0">
+    <div className="max-w-7xl mx-auto py-3 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+      <div className="flex justify-center space-x-6 md:order-2">
+        {navigation.map((item) => (
+          <a key={item.name} href={item.href} className="text-white hover:text-gray-500">
+            <span className="sr-only">{item.name}</span>
+            <item.icon className="h-6 w-6" aria-hidden="true" />
+          </a>
+        ))}
+      </div>
+      <div className="mt-8 md:mt-0 md:order-1">
+        <p className="text-center text-base text-white">&copy; 2022 Jose&apos;s Fun API. All rights reserved.</p>
+      </div>
+    </div>
+  </footer>
     )
   }
   
